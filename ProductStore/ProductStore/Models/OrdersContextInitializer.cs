03f61c3a19878d56c42dc0e2588870f3791e2d4f
Yang Rui <ryang@thoughtworks.com>
2013-12-24 00:06:55 +0800

    ProductStore

---
+++ ProductStore/ProductStore/Models/OrdersContextInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProductStore.Models
+{
+    public class OrdersContextInitializer : DropCreateDatabaseIfModelChanges<OrdersContext>
+    {
+        protected override void Seed(OrdersContext context)
+        {
+            var products = new List<Product>()            
+            {
+                new Product() { Name = "ShuXue", PageCount = 100, ActualCost = .99M },
+                new Product() { Name = "YuWen", PageCount = 150, ActualCost = 10 },
+                new Product() { Name = "ZhengZhi", PageCount = 200, ActualCost = 2.05M }
+            };
+
+            products.ForEach(p => context.Products.Add(p));
+            context.SaveChanges();
+
+            var order = new Order() { Customer = "Bob" };
+            var od = new List<OrderDetail>()
+            {
+                new OrderDetail() { Product = products[0], Quantity = 2, Order = order},
+                new OrderDetail() { Product = products[1], Quantity = 4, Order = order }
+            };
+            context.Orders.Add(order);
+            od.ForEach(o => context.OrderDetails.Add(o));
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ ProductStore/ProductStore/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductStore.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public decimal PageCount { get; set; }
+        public decimal ActualCost { get; set; }
+    }
+}
+
+namespace ProductStore.Models
+{
+}
+
+namespace ProductStore.Models
+{
+}
