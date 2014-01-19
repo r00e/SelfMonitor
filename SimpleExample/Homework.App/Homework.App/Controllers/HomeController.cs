2ad81e2215a83a3bc67c1b05c597275f6990f274
Yang Rui <ryang@thoughtworks.com>
2014-01-19 23:33:05 +0800

    remove database close

--- SimpleExample/Homework.App/Homework.App/Controllers/HomeController.cs
+++ SimpleExample/Homework.App/Homework.App/Controllers/HomeController.cs
@@ -82,8 +82,6 @@ namespace Homework.App.Controllers
                             records.Last().Date = (DateTime)reader["Date"];
                         }
                     }
-
-                    con.Close();
                 }
             }
 
