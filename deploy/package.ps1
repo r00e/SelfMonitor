4ff8fd6ba2753ee48f17cadc483db8fad6c2f69a
Yang Rui <ryang@thoughtworks.com>
2014-01-21 22:58:07 +0800

    add create-website.ps1

--- deploy/package.ps1
+++ deploy/package.ps1
@@ -1,8 +1,21 @@
 if(Test-path .\package){
+    write-host "remove existed package"
     rm -r -force .\package
+    write-host "create new package"
+    mkdir .\package
 }
 else{
+    write-host "create new package"
     mkdir .\package
 }
 
+write-host "copy bin folder"
 cp -r ..\SimpleExample\Homework.App\Homework.App\bin .\package
+write-host "copy Views"
+cp -r ..\SimpleExample\Homework.App\Homework.App\Views .\package
+write-host "copy packages.config"
+cp ..\SimpleExample\Homework.App\Homework.App\packages.config .\package
+write-host "copy Global.asax"
+cp ..\SimpleExample\Homework.App\Homework.App\Global.asax .\package
+write-host "copy web.config"
+cp ..\simpleExample\homework.app\homework.app\web.config .\package
