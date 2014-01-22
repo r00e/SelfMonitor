34f781207067b28363829cf394c6d2ea231c4645
Yang Rui <ryang@thoughtworks.com>
2014-01-22 11:07:00 +0800

    auto-create acc and set authentication mode for SQL Server

--- deploy/sql/create-db.sql
+++ deploy/sql/create-db.sql
@@ -2,13 +2,10 @@ IF EXISTS (SELECT name
            FROM   sys.databases 
            WHERE  name = N'HanHomework') 
   DROP DATABASE hanhomework 
-
 CREATE DATABASE hanhomework 
-
-go 
+GO 
 
 USE hanhomework 
-
 CREATE TABLE data 
   ( 
      [name] VARCHAR(50), 
@@ -16,5 +13,28 @@ CREATE TABLE data
      [page] VARCHAR(1000), 
      [date] DATE 
   ) 
+GO 
 
-go 
+USE [master]
+GO
+
+EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'LoginMode', REG_DWORD, 2
+GO
+
+USE [master]
+GO
+
+CREATE LOGIN [handb] WITH PASSWORD=N'123456', DEFAULT_DATABASE=[hanhomework], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
+
+USE [hanhomework]
+GO
+
+CREATE USER [handb] FOR LOGIN [handb]
+GO
+
+USE [hanhomework]
+GO
+
+EXEC sp_addrolemember N'db_owner', N'handb'
+GO
