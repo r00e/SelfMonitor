c94131fece95c917b5831d7fdf35ff384d073ea0
Yang Rui <ryang@thoughtworks.com>
2014-01-26 11:25:40 +0800

    update deploy script

--- deploy/create-website.ps1
+++ deploy/create-website.ps1
@@ -1,19 +1,13 @@
 Import-Module webadministration
 
 if(test-path IIS:\AppPools\DemoAppPool){
-    Remove-Item -r -force IIS:\AppPools\DemoAppPool
+    Remove-WebAppPool DemoAppPool
 }
-New-Item -force IIS:\AppPools\DemoAppPool
+New-WebAppPool DemoAppPool
 Set-ItemProperty IIS:\AppPools\DemoAppPool managedRuntimeVersion v4.0
 
 if(test-path IIS:\Sites\DemoSite){
-    Remove-Item -r -force IIS:\Sites\DemoSite
+    Remove-WebSite -Name "DemoSite"
 }
-New-Item -force IIS:\Sites\DemoSite -physicalPath C:\my\SelfMonitor\deploy\package -bindings @{protocol="http";bindingInformation=":9999:"}
+New-WebSite -Name DemoSite -PhysicalPath C:\my\SelfMonitor\deploy\package -Port 9999 
 Set-ItemProperty IIS:\Sites\DemoSite -name applicationPool -value DemoAppPool
-
-# New-Item IIS:\Sites\DemoSite\DemoApp -physicalPath C:\DemoSite\DemoApp -type Application
-# Set-ItemProperty IIS:\sites\DemoSite\DemoApp -name applicationPool -value DemoAppPool
-
-# New-Item IIS:\Sites\DemoSite\DemoVirtualDir1 -physicalPath C:\DemoSite\DemoVirtualDir1 -type VirtualDirectory
-# New-Item IIS:\Sites\DemoSite\DemoApp\DemoVirtualDir2 -physicalPath C:\DemoSite\DemoVirtualDir2 -type VirtualDirectory
--- deploy/package.ps1
+++ deploy/package.ps1
@@ -1,21 +1,51 @@
-if(Test-path .\package){
-    write-host "remove existed package"
-    rm -r -force .\package
-    write-host "create new package"
-    mkdir .\package
-}
-else{
-    write-host "create new package"
-    mkdir .\package
+$packagePath = ".\package"
+
+$srcFile = @{
+    "binFolder" = "..\SimpleExample\Homework.App\Homework.App\bin";
+    "viewsFolder" = "..\SimpleExample\Homework.App\Homework.App\Views";
+    "packagesConfig" = "..\SimpleExample\Homework.App\Homework.App\packages.config";
+    "globalAsax" = "..\SimpleExample\Homework.App\Homework.App\Global.asax";
+    "webConfig" = "..\simpleExample\homework.app\homework.app\web.config";
 }
 
-write-host "copy bin folder"
-cp -r ..\SimpleExample\Homework.App\Homework.App\bin .\package
-write-host "copy Views"
-cp -r ..\SimpleExample\Homework.App\Homework.App\Views .\package
-write-host "copy packages.config"
-cp ..\SimpleExample\Homework.App\Homework.App\packages.config .\package
-write-host "copy Global.asax"
-cp ..\SimpleExample\Homework.App\Homework.App\Global.asax .\package
-write-host "copy web.config"
-cp ..\simpleExample\homework.app\homework.app\web.config .\package
+Function DeleteIfExisted($target){
+    if(Test-path $target){
+        write-host "remove existed $target"
+        rm -r -force $target
+    }
+}
+
+Function CreateFolder($folder){
+    write-host "create new $folder"
+    mkdir $folder
+}
+
+Function CopySrcFileTo($dest) {
+    $srcFile[$srcFile.keys] | `
+    %{ if((Get-Item $_).PSIsContainer){
+            cp -r $_ $dest
+        } 
+        else{
+            cp $_ $dest
+        }
+    }
+}
+
+Function CreatePackage {
+    DeleteIfExisted -target $packagePath
+    CreateFolder -folder $packagePath
+    CopySrcFileTo -dest $packagePath
+}
+
+CreatePackage
+
+# write-host "copy bin folder"
+# cp -r ..\SimpleExample\Homework.App\Homework.App\bin .\package
+# write-host "copy Views"
+# cp -r ..\SimpleExample\Homework.App\Homework.App\Views .\package
+# write-host "copy packages.config"
+# cp ..\SimpleExample\Homework.App\Homework.App\packages.config .\package
+# write-host "copy Global.asax"
+# cp ..\SimpleExample\Homework.App\Homework.App\Global.asax .\package
+# write-host "copy web.config"
+# cp ..\simpleExample\homework.app\homework.app\web.config .\package
