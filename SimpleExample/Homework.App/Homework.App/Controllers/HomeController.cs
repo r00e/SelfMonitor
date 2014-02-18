093b9f860f7a324fcb0413f110fa74c0ab3993b6
Yang Rui <ryang@thoughtworks.com>
2014-02-18 17:26:50 +0800

    edit row; delete row; save into DB

--- SimpleExample/Homework.App/Homework.App/Controllers/HomeController.cs
+++ SimpleExample/Homework.App/Homework.App/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using Homework.App.Models;
 
@@ -33,6 +34,57 @@ namespace Homework.App.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpPost]
+        public Record UpdateRecord(Record record, string type)
+        {
+            string value = null;
+            switch (type)
+            {
+                case "name":
+                    value = record.Name;
+                    break;
+                case "book":
+                    value = record.Book;
+                    break;
+                case "page":
+                    value = record.Page;
+                    break;
+            }
+
+            var connectionString = ConfigurationManager.ConnectionStrings["HanHomeworkConnectStr"].ConnectionString;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var deleteRecord = String.Format("UPDATE [hanhomework].[dbo].[data] SET {0} = '{1}' WHERE Id = {2} ", type, value, record.Id);
+
+                var command = new SqlCommand(deleteRecord, connection);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            return record;
+        }
+        
+        [HttpPost]
+        public int DeleteRecord(Record record)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["HanHomeworkConnectStr"].ConnectionString;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var deleteRecord = String.Format("DELETE FROM data WHERE Id = {0} ", record.Id);
+
+                var command = new SqlCommand(deleteRecord, connection);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            return record.Id;
+        }
+
+
         private List<Record> GetAllRecords()
         {
             var records = new List<Record>();
@@ -49,6 +101,7 @@ namespace Homework.App.Controllers
                     while (dataReader.Read())
                     {
                         records.Add(new Record());
+                        records.Last().Id = (int) dataReader["Id"];
                         records.Last().Name = (string) dataReader["Name"];
                         records.Last().Book = (string) dataReader["Book"];
                         records.Last().Page = (string) dataReader["Page"];
--- SimpleExample/Homework.App/Homework.App/Models/Record.cs
+++ SimpleExample/Homework.App/Homework.App/Models/Record.cs
@@ -4,6 +4,7 @@ namespace Homework.App.Models
 {
     public class Record
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Book { get; set; }
         public string Page { get; set; }
